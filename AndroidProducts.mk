ae8a93a7cd64d18848bf8df5ecf1ef3410832be9
Edward Wu <bluehome.wu@gmail.com>
2021-10-31 10:58:53 +0000

    J607F/J607Z: Init TWRP Device Tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_J607Z.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_J607Z-user \
+    omni_J607Z-userdebug \
+    omni_J607Z-eng
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_J607Z-user
+add_lunch_combo omni_J607Z-userdebug
+add_lunch_combo omni_J607Z-eng
